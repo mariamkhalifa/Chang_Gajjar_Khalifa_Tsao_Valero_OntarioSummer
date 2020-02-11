38f41b7bd55088aea1c4fd807b06228d06c8a891
kchang00 <k_chang@fanshaweonline.ca>
2020-02-11 06:24:15 -0500

    hero style fixes, nav fixes, separate php files

---
+++ nav.php
@@ -0,0 +1,10 @@
+<header class="header">
+    <router-link to="/" class="logo"><img src="public/images/logo_v2.svg" alt="logo"></router-link>
+    <i v-on:click="expandBurger" class="fa fa-bars fa-2x" :class="{'rotated':burger.isExpanded}"></i>
+    <ul class="nav" :class="{'visible':burger.isExpanded}">
+        <li class="nav-link"><router-link to="/">Home</router-link></li>
+        <li class="nav-link"><a href="#about">About</a></li>
+        <li class="nav-link"><a href="#destinations">Destinations</a></li>
+        <li class="nav-link"><a href="includes/admin/signup.php">Sign Up</a></li>
+    </ul>
+</header>
