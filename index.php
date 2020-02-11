54854ef5a0e53682059c6a7f1b65cc74e3382804
kchang00 <k_chang@fanshaweonline.ca>
2020-02-11 07:39:29 -0500

    favicon fix

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" type="image/x-icon" href="/public/images/favicon.ico">
+    <link rel="icon" type="image/x-icon" href="public/images/favicon.ico">
     <link rel="stylesheet" href="https://use.typekit.net/fzo1xgx.css">
     <link rel="stylesheet" href="public/css/main.css">
     <script src="https://unpkg.com/vue/dist/vue.js"></script>
